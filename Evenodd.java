89704ec9a19e7555231b0ece4b0128e05fd017d5
SHWETA JHA <150259603+U4Universe@users.noreply.github.com>
2024-07-04 11:31:07 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,12 @@
+class Arithimetic
+{
+  public static void main(String args[])
+   {
+     int a;
+     a=Integer.parseInt(args[0]);
+     if(a%5==0)
+       System.out.println(a+" is divisble by 5");
+     else
+       System.out.println(a+" is not divisble by 5");
+    }
+}
---
+++ Msg.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+class Msg
+  {
+    public static void main(String arg[])
+     {
+       System.out.println("Hello World,from Shweta Jha");
+     }
+  }
